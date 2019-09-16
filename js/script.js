421fcaa80bab19dd62064309cc231a158daf5802
Cody <54541144+CodyDickson@users.noreply.github.com>
2019-09-16 14:24:28 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// Main Menu and Game Set Up
+function startGame() {
+    document.getElementById("welcome").style.display = "none";
+    document.getElementById("gameBoard").style.display = "block";   
+}
+
+// Main Gameplay Loop
+
+// Attack (1 Dice, 2 Dice)
+// Select a target on the opponent's side
+
+// Draw Power Card (If current Power Cards < 5)
+// +1 to Dice Roll, Add another castle (max = 3), +1 to Castle Health
+
+// Use a Power Card
+
+// Clear Power Cards
+
+// Opponent's Turn
+
+// End the Game / Start a New Game
